76449baa0a73d1b45c537afd8c0dfeb95e183170
Barry Hood <BarryPH@protonmail.com>
2017-07-02 00:00:42 +1200

    Load font 'Anonymous Pro' from google fonts

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,13 @@
 import GlobalStyles from '../css/global.js';
 import Header from './Header';
+import Head from 'next/head';
 
 const Layout = (props) => (
 	<div>
+		<Head>
+			<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Anonymous+Pro' />
+		</Head>
+
 		<Header />
 
 		<main>
